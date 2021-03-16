a3f237458c6b6ca1fddb919de08a804e125b3e7a
Marlena Bannick <mnorwood@uw.edu>
2021-03-15 22:53:17 -0700

    added simulations

--- simulations/simulation-submit.sh
+++ simulations/simulation-submit.sh
@@ -9,9 +9,52 @@ SCRIPT="${HOME}/repos/biost571-final/simulations/simulation-worker.R"
 
 CONSTANTS="-cwd -N sim571 -j y -o ${SGEOUT} -pe smp 1 -q normal.q ${SHELL} ${SCRIPT}"
 
+FITY="exchangeable"
+LINK="identity"
+
+ARGS="-corstr ${FITY} -fit_link ${FITY}"
+
+CONSTRAIN="-rho 100"
+
 # This will do the number of simulations
 # passed to this script as the first arg
-for i in $seq(1 1 $1)
+for i in $seq($1 1 $2)
 do
-  qsub ${CONSTANTS} # ADD IN MORE ARGUMENTS THROUGH FOR LOOPS
+  ARGS="${ARGS} -sim ${i}"
+  for N in 100 250
+  do
+    ARGS="${ARGS} -n_sub ${N}"
+    for MAXOBS in 1 5
+    do
+      MINOBS=$((${MAXOBS}+5))
+      ARGS="${ARGS} -min_obs ${MINOBS} -max_obs ${MAXOBS}"
+      for CORX in independence block
+      do
+        ARGS="${ARGS} -corr_X ${CORX}"
+        for ALPHAY in 0.0 0.5
+        do
+          ARGS="${ARGS} -alpha_obs ${ALPHAY}"
+          for NPRED in 40 80
+          do
+            ARGS="${ARGS} -n_pred ${NPRED}"
+
+            qsub ${CONSTANTS} ${ARGS}
+            qsub ${CONSTANTS} ${ARGS} ${CONSTRAIN}
+
+            for GAMMA in 1 2
+            do
+              PEN="-gamma ${GAMMA}"
+              for LAMBDA in 0.5 1 1.5 2
+              do
+                PEN="${PEN} -lambda ${LAMBDA}"
+
+                qsub ${CONSTANTS} ${ARGS} ${PEN}
+                qsub ${CONSTANTS} ${ARGS} ${PEN} ${CONSTRAIN}
+              done
+            done
+          done
+        done
+      done
+    done
+  done
 done
--- simulations/simulation-worker.R
+++ simulations/simulation-worker.R
@@ -10,6 +10,7 @@ library(R.utils)
 
 setwd("~/repos/biost571-final")
 source("fit-gee.R")
+source("data-continuous.R")
 
 # GET COMMAND-LINE ARGUMENTS ----------------------
 
@@ -20,16 +21,24 @@ args <- commandArgs(trailingOnly=TRUE, asValues=TRUE,
                       sim=1,
 
                       # ADD IN ARGUMENTS FOR SIMULATION
+                      n_sub=50,
+                      min_obs=1,
+                      max_obs=6,
+                      n_pred=40,
+                      sigma_X=1,
+                      sigma_Y=2,
+                      corr_X='independence',
+                      beta_mean=2,
+                      alpha_obs=0.0,
 
                       # THESE ARE ARGUMENTS FOR MODEL FITTING
 
                       fit_link='identity',
                       corstr='independence',
-                      constrain=FALSE,
                       rho=NULL,
                       gamma=NULL,
                       lambda=NULL,
-                      tol=1e-15,
+                      tol=1e-12,
                       maxiter=500,
                       verbose=FALSE,
                       directory="."
@@ -39,8 +48,10 @@ args <- commandArgs(trailingOnly=TRUE, asValues=TRUE,
 
 if(is.null(args$rho)){
   args$rho <- NA
+  args$constrain <- FALSE
 } else {
   args$rho <- as.numeric(args$rho)
+  args$constrain <- TRUE
 }
 
 if(is.null(args$gamma)){
@@ -55,33 +66,113 @@ if(is.null(args$lambda)){
   args$lambda <- as.numeric(args$lambda)
 }
 
+if(args$corr_X == "block") args$corr_X <- "block diagonal"
+
 # SIMULATE DATA -----------------------------------
 
 # Set a reproducible seed
 set.seed(args$sim)
 
+bl.size <- 10
+alpha_X <- 0.5
+
 # Simulate data
-# ...
+corr_X <- build_corr_X(n_pred=args$n_pred,
+                       alpha=alpha_X,
+                       corstr=args$corr_X,
+                       blocksize=rep(bl.size, args$n_pred/bl.size))
+
+# Simulate some betas
+b.N <- rnorm(n=args$n_pred/4, mean=args$beta_mean)
+b.N2 <- -b.N
+b.Y <- rep(0, args$n_pred/2)
+betas <- c(b.N, b.N2, b.Y)
+betas <- sample(betas)
+
+if(args$fit_link == "identity"){
+
+  data <- sim_normal(
+    n_sub=args$n_sub,
+    min_obs=args$min_obs, max_obs=args$max_obs,
+    n_pred=args$n_pred,
+    sigma_X=args$sigma_X,
+    sigma_Y=args$sigma_Y,
+    corr_X=corr_X,
+    alpha_obs=args$alpha_obs,
+    beta=betas
+  )
+
+  y <- data$Y
+  X <- cbind(rep(1, length(y)), data$X)
+  id <- data$id
+
+}
 
 # FIT MODEL ---------------------------------------
 
 # Fit the GEE model with arguments to simulated data
 fit <- fit.gee(y=y, X=X, id=id,
-               link=args$link, corstr=args$corstr,
+               link=args$fit_link, corstr=args$corstr,
                constrain=args$constrain,
                rho=args$rho, gamma=args$gamma,
                lambda=args$lambda,
                tol=args$tol, maxiter=args$maxiter,
                verbose=args$verbose)
 
+# FIT MODEL W/ CROSS-VALIDATION -------------------
+
+# Sample 70% of the individuals
+unique.ids <- unique(id)
+iss <- sample(unique.ids, size=0.7*length(unique.ids))
+indices.is <- id %in% iss
+indices.os <- !id %in% iss
+
+id.is <- id[indices.is]
+y.is <- y[indices.is]
+X.is <- X[indices.is,]
+
+fit.oos <- fit.gee(y=y.is, X=X.is, id=id.is,
+                   link=args$fit_link, corstr=args$corstr,
+                   constrain=args$constrain,
+                   rho=args$rho, gamma=args$gamma,
+                   lambda=args$lambda,
+                   tol=args$tol, maxiter=args$maxiter,
+                   verbose=args$verbose)
+
+y.os <- y[indices.os]
+X.os <- X[indices.os, ]
+
 # COMPUTE EMPIRICAL PERFORMANCE METRICS -----------
 
-# Decide what we want to compute, i.e. bias, coverage, MSE, etc.
-# ...
+true.beta <- c(0, betas)
 
-bias <- 0
-coverage <- FALSE
-mse <- 0
+# Compute either MSE or classification rate
+if(args$fit_link == "identity"){
+
+  residual.is <- y - X %*% fit$beta
+  mse.is <- mean(residual.is**2)**0.5
+
+  residual.os <- y.os - X.os %*% fit$beta
+  mse.os <- mean(residual.os**2)**0.5
+
+} else {
+
+  nu.is <- exp(X %*% fit$beta)/(1 + exp(X %*% fit$beta))
+  class.is <- nu.is >= 0.5
+  mse.is <- sum(class.is == y) / length(y)
+
+  nu.os <- exp(X.os %*% fit$beta)/(1 + exp(X.os %*% fit$beta))
+  class.os <- nu.os >= 0.5
+  mse.os <- sum(class.os == y.os) / length(y.os)
+
+}
+
+# Compute the percent of correctly zeroed out predictors
+
+beta0.true <- true.beta <= 1e-15
+beta0.fit <- fit$beta <= 1e-15
+
+pct.0 <- mean(beta0.true == beta0.fit)
 
 # SAVE RESULTS ------------------------------------
 
@@ -89,7 +180,20 @@ mse <- 0
 df.results <- data.table(
 
   # REPRODUCIBLE SEED
-  sim_id=sim,
+  sim_id=args$sim,
+
+  # DATA SIMULATION SETTINGS
+  # ADD IN ARGUMENTS FOR SIMULATION
+  n_sub=args$n_sub,
+  min_obs=args$min_obs,
+  max_obs=args$max_obs,
+  n_pred=args$n_pred,
+  sigma_X=args$sigma_X,
+  sigma_Y=args$sigma_Y,
+  corr_X=args$corr_X,
+  beta_mean=args$beta_mean,
+  alpha_obs=args$alpha_obs,
+  snr=c(data$SNR),
 
   # MODEL SETTINGS
   fit_link=args$link,
@@ -114,9 +218,9 @@ df.results <- data.table(
 
   # EMPIRICAL PERFORMANCE
 
-  bias=bias,
-  coverage=coverage,
-  mse=mse
+  mse_is=mse.is,
+  mse_os=mse.os,
+  pct0=pct.0
 )
 
 # Create a unique filename for the simulation
